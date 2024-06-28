b487d280d53d67d6749956583bc933c710a96478
razapanjwani <razapanjwani16986@gmail.com>
2024-06-28 14:55:34 +0500

    run service4 on docker

--- Inventory_Service/service4/main.py
+++ Inventory_Service/service4/main.py
@@ -1,32 +1,13 @@
 from fastapi import FastAPI
 from fastapi import FastAPI, Depends, HTTPException
 from typing import Annotated
-from contextlib import asynccontextmanager
 from sqlmodel import SQLModel, create_engine, Session, select
 from fastapi import FastAPI
 from service4.models import *
-from service4.setting import DATABASE_URL
 from service4.service import *
+from service4.database import *
 
-connection_string = str(DATABASE_URL)
 
-engine = create_engine(
-    connection_string, connect_args={}, pool_recycle=300
-)
-
-# Create the tables
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Creating database connection")
-    create_db_and_tables()
-    yield
-
-def db_session():
-    with Session(engine) as session:
-        yield session
 
 app = FastAPI(
     title="Inventory Service",

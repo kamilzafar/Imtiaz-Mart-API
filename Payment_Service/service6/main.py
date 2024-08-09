55582a19effb979617611cb09a3dcdaddd034c7c
razapanjwani <razapanjwani16986@gmail.com>
2024-08-09 15:58:09 +0500

    added webhooks

--- Payment_Service/service6/main.py
+++ Payment_Service/service6/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status,Request
 from fastapi.security import OAuth2PasswordBearer
 from service6.db import create_db_and_tables
 from contextlib import asynccontextmanager
 from service6.service import *
+import json
+import os
+import stripe
 
 app = FastAPI(
     title="Payment Service",
@@ -18,6 +21,7 @@ async def lifespan(app: FastAPI):
     yield
 
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+stripe.api_key = settings.SECRET_KEY_STRIPE
 
 
 @app.get("/", tags=["Root"])
@@ -31,3 +35,43 @@ def process_payment(order_id:int):
     payment = generate_checkout_session(order_id)
     return payment
 
+
+
+
+# This is your Stripe CLI webhook secret for testing your endpoint locally.
+endpoint_secret = settings.WB_SECRET_KEY
+
+
+@app.post('/webhook')
+def webhook(request:Request):
+    event = None
+    payload = request.body
+    sig_header = request.headers['STRIPE_SIGNATURE']
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        raise e
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        raise e
+
+    # Handle the event
+    if event['type'] == 'payment_intent.succeeded':
+      payment_intent = event['data']['object']
+    if event['type'] == 'checkout.session.async_payment_failed':
+      session = event['data']['object']
+    elif event['type'] == 'checkout.session.async_payment_succeeded':
+      session = event['data']['object']
+    elif event['type'] == 'checkout.session.completed':
+      session = event['data']['object']
+    elif event['type'] == 'checkout.session.expired':
+      session = event['data']['object']
+    # ... handle other event types
+    else:
+      print('Unhandled event type {}'.format(event['type']))
+
+    return json.load(success=True)
--- Payment_Service/service6/settings.py
+++ Payment_Service/service6/settings.py
@@ -7,3 +7,5 @@ except FileNotFoundError:
 
 DATABASE_URL = config("DATABASE_URL", cast=str)
 ORDER_SERVICE_URL = config("ORDER_SERVICE_URL", cast=str)
+SECRET_KEY_STRIPE = config("SECRET_KEY_STRIPE",cast=str)
+WB_SECRET_KEY = config("WB_SECRET_KEY",cast=str)
